93e942b9b479fc5e0b43e6a856c1bc0982d70734
Udara Neminda <udaraneminda@gmail.com>
2024-10-06 14:25:18 +0530

    main-file

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Animal d1 = new Dog();
+        Animal c1 = new Cat();
+
+        c1.sound();
+        d1.sound();
+    }
+}
